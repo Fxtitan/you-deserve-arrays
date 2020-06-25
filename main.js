f130b3baea921e4795153e9256c85bf83a02dbfb
Sharod Bailey <Sharod.Bailey@codeimmersives.comy>
2020-06-25 15:30:49 -0400

    almost

--- main.js
+++ main.js
@@ -1,6 +1,43 @@
 /******************
  * YOUR CODE HERE *
  ******************/
+getFirstItemFrom = (arr) => {
+  return arr[0];
+}
+
+getLastItemFrom = (arr) => {
+  return arr[arr.length -1];
+}
+
+getIndex3 = (arr) => {
+  if(arr.length === 5) {
+    return arr[3]
+  } else {
+    return arr[arr.length -1];
+  }
+}
+
+isLongList = (arr) => {
+  if (arr.length >= 11 || arr.length === 10) {
+    return true;
+  } else if (arr.length < 10) {
+    return false;
+  }
+}
+
+firstItemIsNumber = (arr) => {
+  if (typeof arr[0] === 'number') {
+    return true;
+  } if (typeof arr[0] === 'string' || typeof arr[0] === 'boolean') {
+    return false;
+  } if (arr[0] === arr.isArray([1, 2, 3])) {
+return false
+  }
+}
+
+secondCharOfThirdString = (char) => {
+
+}
 
 
 
@@ -8,7 +45,11 @@
 
 
 
- /********************************************
+
+
+
+
+/********************************************
  * CODE DOWN HERE IS FOR INTERNAL USE ONLY. *
  *           PLEASE DON'T TOUCH!            *
  ********************************************/
